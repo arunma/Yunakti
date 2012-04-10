765fe2eba60f2c9d4170a407fcae44653b187571
Arun Manivannan <arun@arunma.com>
2012-04-10 22:26:45 +0800

    Initial Checkin
    Initial Checkin

---
+++ test/sg/edu/nus/iss/yunakti/engine/parser/TestSample.java
@@ -0,0 +1,15 @@
+package sg.edu.nus.iss.yunakti.engine.parser;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestSample {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
